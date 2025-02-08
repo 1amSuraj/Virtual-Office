c2890ba077fc2ef5ee5238cf87219192d4987999
1amSuraj <srisuraj41@gmail.com>
2025-02-08 16:18:42 +0530

    hello

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-tyfiyfi
-dfg
-fg
-dfg
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
